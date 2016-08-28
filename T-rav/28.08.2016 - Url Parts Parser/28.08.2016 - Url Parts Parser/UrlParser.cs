c750f6a6f8b6247912b5bd67953b8f412ae570f2
Travis Frisinger <tmfrisinger@gmail.com>
2016-08-28 20:38:14 +0400

    Url Parts Kata - 80% done. Port and Path left

---
+++ T-rav/28.08.2016 - Url Parts Parser/28.08.2016 - Url Parts Parser/UrlParser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _28._08._2016___Url_Parts_Parser
+{
+    public class UrlParser
+    {
+        public UrlParts Parse(string url)
+        {
+            var protocolString = GetProtocol(url);
+            var subdomainString = GetSubdomain(url);
+            var domainNameString = GetDomainName(url);
+            return new UrlParts(protocolString, subdomainString, domainNameString);
+        }
+
+        private static string GetDomainName(string url)
+        {
+            var domainName = url.SkipWhile(c => c != '/').Skip(2).TakeWhile(c => c != '.').ToArray();
+            if (HasSubdomain(url))
+            {
+                domainName = url.SkipWhile(c => c != '.').Skip(1).TakeWhile(c => c != '.').ToArray();
+            }
+            var domainNameString = new string(domainName);
+            return domainNameString;
+        }
+
+        private string GetProtocol(string url)
+        {
+            var protocol = url.TakeWhile(c => c != ':');
+            var protocolString = new string(protocol.ToArray());
+            return protocolString;
+        }
+
+        private string GetSubdomain(string url)
+        {
+            var subdomainString = string.Empty;
+            if (HasSubdomain(url))
+            {
+                var subdomain = url.SkipWhile(c => c != '/').Skip(2).TakeWhile(c => c != '.').ToArray();
+                subdomainString = new string(subdomain);
+            }
+            return subdomainString;
+        }
+
+        private static bool HasSubdomain(string url)
+        {
+            return url.IndexOf(".", StringComparison.InvariantCulture) != url.LastIndexOf(".", StringComparison.InvariantCulture);
+        }
+    }
+
+    public class UrlParts
+    {
+        public UrlParts(string protocol, string subdomain, string domainName)
+        {
+            Protocol = protocol;
+            Subdomain = subdomain;
+            DomainName = domainName;
+        }
+
+        public string Protocol { get; private set; }
+        public string Subdomain { get; private set; }
+        public string DomainName { get; private set; }
+    }
+}
---
+++ T-rav/28.08.2016 - Url Parts Parser/UrlPartsParserTests/UrlParserTests.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using _28._08._2016___Url_Parts_Parser;
+
+namespace UrlPartsParserTests
+{
+    [TestFixture]
+    public class UrlParserTests
+    {
+
+        [Test]
+        public void Parse_WhenUrlContainsHttpProtocol_ShouldReturnHttpForProtocol()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "http";
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("http://www.google.com");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Protocol);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsFtpProtocol_ShouldReturnFtpForProtocol()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "ftp";
+            var parser = new UrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("ftp://kungfuactiongrip.com");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Protocol);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsHttpsProtocol_ShouldReturnHttpsForProtocol()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "https";
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("https://bar.com");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Protocol);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsNoSubdomain_ShouldReturnEmptyStringForSubdomain()
+        {
+            //---------------Set up test pack-------------------
+            var expected = string.Empty;
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("https://takealot.com");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Subdomain);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsWwwSubdomain_ShouldReturnWwwSubdomain()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "www";
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("https://www.goodstuff.com");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Subdomain);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsBarSubdomain_ShouldReturnBarSubdomain()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "bar";
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("ftp://bar.foo.com");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Subdomain);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsSubdomainAndFoobarDomainName_ShouldReturnFoobarDomainName()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "foobar";
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("sftp://stuff.foobar.net");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.DomainName);
+        }
+
+        [Test]
+        public void Parse_WhenUrlContainsNoSubDomainAndMooDomainName_ShouldReturnMooDomainName()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "moo";
+            var parser = CreateUrlParser();
+            //---------------Execute Test ----------------------
+            var result = parser.Parse("http://moo.co");
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.DomainName);
+        }
+
+
+        private UrlParser CreateUrlParser()
+        {
+            var parser = new UrlParser();
+            return parser;
+        }
+
+        //[Test]
+        //public void Parse_WhenUrlNoSubdomain_ShouldEmptyStringForSubdomain()
+        //{
+        //    //---------------Set up test pack-------------------
+        //    var parser = new UrlParser();
+        //    //---------------Assert Precondition----------------
+
+        //    //---------------Execute Test ----------------------
+        //    var result = parser.Parse("http://bar.com");
+        //    //---------------Test Result -----------------------
+        //    Assert.AreEqual(string.Empty, result[1]);
+        //}
+    }
+}
