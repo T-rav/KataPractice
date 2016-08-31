d23292b3e8b775724bbd7d9bce82e215513e653a
Travis Frisinger <tmfrisinger@gmail.com>
2016-08-31 20:48:57 +0400

    Numbers to Words Kata

---
+++ T-rav/Numbers to Words/Numbers to Words/NumberToWordConverter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Numbers_to_Words
+{
+    public class NumberToWordConverter
+    {
+        private readonly IDictionary<int, string> _lookupDigits = new Dictionary<int, string>()
+        {
+            {0, "zero"},
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {9, "nine"},
+
+            {10,"ten"},
+            {20,"twenty"},
+            {30,"thirty"},
+            {40,"fourty"},
+            {50,"fifty"},
+            {60,"sixty"},
+            {70,"seventy"},
+            {80,"eighty"},
+            {90,"ninety"},
+        };
+
+        public string Convert(int input)
+        {
+            if (HasDirectNumberToStringTranslation(input))
+            {
+                return GetNumberAsString(input);
+            }
+
+            var oneDigitKey = input%10;
+            var twoDigitKey = input - oneDigitKey;
+
+            return GetNumberAsString(twoDigitKey)+"-"+GetNumberAsString(oneDigitKey);
+        }
+
+        private string GetNumberAsString(int input)
+        {
+            return _lookupDigits[input];
+        }
+
+        private bool HasDirectNumberToStringTranslation(int input)
+        {
+            return _lookupDigits.ContainsKey(input);
+        }
+    }
+}
---
+++ T-rav/Numbers to Words/Numbers to Words/NumberToWordConverterTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+
+namespace Numbers_to_Words
+{
+    [TestFixture]
+    public class NumberToWordConverterTests
+    {
+
+        [Test]
+        public void Convert_WhenNumberZero_ShouldReturnTextZero()
+        {
+            //---------------Set up test pack-------------------
+            var numberToWordConverter = CreateNumberToWordConverter();
+            //---------------Execute Test ----------------------
+            var result = numberToWordConverter.Convert(0);
+            //---------------Test Result -----------------------
+            Assert.AreEqual("zero",result);
+        }
+
+        private NumberToWordConverter CreateNumberToWordConverter()
+        {
+            var numberToWordConverter = new NumberToWordConverter();
+            return numberToWordConverter;
+        }
+
+        // I wrote three test beyond zero, saw pattern and refactored back into test cases. 
+        // I have not included zero because it is a special case
+
+        [TestCase(1, "one")]
+        [TestCase(2, "two")]
+        [TestCase(3, "three")]
+        [TestCase(4, "four")]
+        [TestCase(5, "five")]
+        [TestCase(6, "six")]
+        [TestCase(7, "seven")]
+        [TestCase(8, "eight")]
+        [TestCase(9, "nine")]
+        public void Convert_WhenNumberSingleNonZeroDigit_ShouldReturnTextOfNumber(int input, string expected)
+        {
+            //---------------Set up test pack-------------------
+            var numberToWordConverter = CreateNumberToWordConverter();
+            //---------------Execute Test ----------------------
+            var result = numberToWordConverter.Convert(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        // had odd spacing where some where ," others where , "
+        // this seemed to cause my brain to register a fault / threat that distracted me
+
+        [TestCase(10, "ten")]
+        [TestCase(20, "twenty")]
+        [TestCase(30, "thirty")]
+        [TestCase(40, "fourty")]
+        [TestCase(50, "fifty")]
+        [TestCase(60, "sixty")]
+        [TestCase(70, "seventy")]
+        [TestCase(80, "eighty")]
+        [TestCase(90, "ninety")]
+        public void Convert_WhenTwoDigitNumberEndingInZero_ShouldReturnTextOfNumberWithoutHyphen(int input, string expected)
+        {
+            //---------------Set up test pack-------------------
+            var numberToWordConverter = CreateNumberToWordConverter();
+            //---------------Execute Test ----------------------
+            var result = numberToWordConverter.Convert(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        // note disharmony with -> // todo : when not in the correct cycle
+
+        [TestCase(21, "twenty-one")]
+        [TestCase(32, "thirty-two")]
+        [TestCase(67, "sixty-seven")]
+        [TestCase(99, "ninety-nine")]
+        public void Convert_WhenTwoDigitNumberNotEndingInZero_ShouldReturnTextWithHyphenatedNumber(int input, string expected)
+        {
+            //---------------Set up test pack-------------------
+            var numberToWordConverter = CreateNumberToWordConverter();
+            //---------------Execute Test ----------------------
+            var result = numberToWordConverter.Convert(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        // 100, 200, 300, 400, etc
+        // 201, 505, 607, 708, etc ( Non hyphen numbers )
+        // 222, 333, 444, 555, etc ( Hyphenated numbers )
+
+    }
+}
