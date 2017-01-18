1273602529f767c38a22dfd9690c0b888f59c421
Travis Frisinger <tmfrisinger@gmail.com>
2017-01-18 20:51:38 +0400

    3 test in on heavy metal bake sale

---
+++ T-rav/Heavy Metal Bake Sale/Heavy Metal Bake Sale Tests/ItemsToPurchaseTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace Heavy_Metal_Bake_Sale_Tests
+{
+    [TestFixture]
+    public class ItemsToPurchaseTests
+    {
+        // todo : seems like a sub vs mock kata?
+
+        [Test]
+        public void GetItemsToPurchase_WhenInvoked_ShouldPrintItemsToPurchase()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Execute Test ----------------------
+            var result = GetItemsToPurchase(string.Empty);
+            //---------------Test Result -----------------------
+            Assert.AreEqual("Items to Pruchase > ", result);
+        }
+
+        [Test]
+        public void GetItemsToPurchase_WhenPurchasingBrownie_ShouldReadB()
+        {
+            //---------------Set up test pack-------------------
+            var consoleLineReader = new ConsoleLineReader("B");
+            //---------------Execute Test ----------------------
+            var result = GetItemsToPurchase(consoleLineReader);
+            //---------------Test Result -----------------------
+            Assert.AreEqual("B", result);
+        }
+        
+        [Test]
+        public void GetItemsToPurchase_WhenPurchasingMuffin_ShouldReadM()
+        {
+            //---------------Set up test pack-------------------
+            var consoleLineReader = new ConsoleLineReader("M");
+            //---------------Execute Test ----------------------
+            var result = GetItemsToPurchase(consoleLineReader);
+            //---------------Test Result -----------------------
+            Assert.AreEqual("M", result);
+        }
+
+        private string GetItemsToPurchase(string inputToReturn)
+        {
+            if (string.IsNullOrEmpty(inputToReturn))
+            {
+                return "Items to Pruchase > ";
+            }
+
+            return inputToReturn;
+        }
+
+        private string GetItemsToPurchase(ConsoleLineReader consoleLineReader)
+        {
+            return consoleLineReader.ReadLine();
+        }
+    }
+
+    public class ConsoleLineReader
+    {
+        private readonly string _lineToRead;
+
+        public ConsoleLineReader(string lineToRead)
+        {
+            _lineToRead = lineToRead;
+        }
+
+        public string ReadLine()
+        {
+            return _lineToRead;
+        }
+    }
+}
