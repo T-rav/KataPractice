b66ee9b6e1c4369cf30e3a853bab926c47517976
Travis Frisinger <tmfrisinger@gmail.com>
2017-01-18 20:37:08 +0400

    Added Heavy Metal Bake Sale

---
+++ T-rav/Heavy Metal Bake Sale/Heavy Metal Bake Sale Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heavy_Metal_Bake_Sale_Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ T-rav/Heavy Metal Bake Sale/Heavy Metal Bake Sale/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heavy_Metal_Bake_Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
